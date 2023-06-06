4afd60c27112f16959ddd0dad36c9ff2a595d038
GabrielaSantos1 <104846859+GabrielaSantos1@users.noreply.github.com>
2023-06-05 21:36:49 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Programa
+{
+    static void Main()
+    {
+        int num = 0;
+
+        do
+        {
+
+            Console.WriteLine("Digite um número de 1 à 10: ");
+            num = int.Parse(Console.ReadLine());
+
+
+        }
+        while (num < 1 || num > 10);
+
+
+
+        Console.WriteLine("Você digitou o número " + num + ".");
+
+
+    }
+
+}
